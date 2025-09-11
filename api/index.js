961d08a9c8b8984d11b77e73e88dd27a346b2794
absensi-guru <smpn13tasikmalaya13@gmail.com>
2025-09-11 13:39:31 +0700

    reset: struktur project siap deploy

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+// Middleware untuk JSON
+app.use(express.json());
+
+// Contoh route utama
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+  res.send("🚀 API Absensi Guru berjalan dengan baik!");
 });
 
+// Bisa tambah route lain
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
+// Export app untuk serverless function
 module.exports = app;
+
